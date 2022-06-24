fe9431c7d13f74eb41544b96f9bf5311c83fc625
wilshan <74950445+wilshan@users.noreply.github.com>
2022-06-24 20:12:39 +0530

    Update applicationstart.sh

--- applicationstart.sh
+++ applicationstart.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+echo 'tcw.James.725' | docker login --username thecloudworld --password-stdin
 docker run -dit -p 5000:5000 --name nameprint 667481606687.dkr.ecr.us-east-1.amazonaws.com/thecloudworld:a632b7e
