9a98a6844779248b07e0735b3570b7b473eaf8f1
wilshan <74950445+wilshan@users.noreply.github.com>
2022-06-24 20:26:33 +0530

    Update applicationstart.sh

--- applicationstart.sh
+++ applicationstart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo 'tcw.James.725' | docker login --username thecloudworld --password-stdin
-docker stack deploy --compose-file docker-compose.yml stackdemo
+docker stack deploy --compose-file docker-stack.yml stack
