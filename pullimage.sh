7c35f00e1dfac6d4b62c01762cab9e1ceb6880b1
wilshan <74950445+wilshan@users.noreply.github.com>
2022-06-24 20:49:51 +0530

    Update pullimage.sh

--- pullimage.sh
+++ pullimage.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 667481606687.dkr.ecr.us-east-1.amazonaws.com
-docker pull 667481606687.dkr.ecr.us-east-1.amazonaws.com/thecloudworld:a632b7e
+docker stack rm stack
